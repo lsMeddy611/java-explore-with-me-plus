6ecaa1cb370ad80fe05da5e66b344cf5a1e140e6
Stanislav <ls.meddy@yandex.ru>
2026-06-21 18:46:06 +0300

    Создание БД, подключение зависмостей для мапстракт, создание базовых Entity моделей для работы с БД через JPA

---
+++ main-service/src/main/java/ru/practicum/model/User.java
@@ -0,0 +1,24 @@
+package ru.practicum.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+}
