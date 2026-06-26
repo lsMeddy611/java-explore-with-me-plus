35461d2072f786a952bd07db6483af6a41e43b51
Stanislav <ls.meddy@yandex.ru>
2026-06-26 17:24:34 +0300

    Добавил ResponseEntity во все имеющиеся контроллеры

--- main-service/src/main/java/ru/practicum/controller/event/PrivateEventController.java
+++ main-service/src/main/java/ru/practicum/controller/event/PrivateEventController.java
@@ -6,6 +6,7 @@ import jakarta.validation.constraints.PositiveOrZero;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.dto.event.EventFullDto;
@@ -26,31 +27,34 @@ public class PrivateEventController {
     private final EventService eventService;
 
     @GetMapping
-    public List<EventShortDto> getUserEvents(@PathVariable Long userId,
-                                             @RequestParam(defaultValue = "0") @PositiveOrZero int from,
-                                             @RequestParam(defaultValue = "10") @Positive int size) {
+    public ResponseEntity<List<EventShortDto>> getUserEvents(@PathVariable Long userId,
+                                        @RequestParam(defaultValue = "0") @PositiveOrZero int from,
+                                        @RequestParam(defaultValue = "10") @Positive int size) {
         log.info("GET /users/{}/events: from={}, size={}", userId, from, size);
-        return eventService.getUserEvents(userId, from, size);
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(eventService.getUserEvents(userId, from, size));
     }
 
     @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
-    public EventFullDto createEvent(@PathVariable Long userId, @Valid @RequestBody NewEventDto newEventDto) {
+    public ResponseEntity<EventFullDto> createEvent(@PathVariable Long userId, @Valid @RequestBody NewEventDto newEventDto) {
         log.info("POST /users/{}/events: {}", userId, newEventDto);
-        return eventService.createEvent(userId, newEventDto);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(eventService.createEvent(userId, newEventDto));
     }
 
     @GetMapping("/{eventId}")
-    public EventFullDto getUserEvent(@PathVariable Long userId, @PathVariable Long eventId) {
+    public ResponseEntity<EventFullDto> getUserEvent(@PathVariable Long userId, @PathVariable Long eventId) {
         log.info("GET /users/{}/events/{}", userId, eventId);
-        return eventService.getUserEvent(userId, eventId);
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(eventService.getUserEvent(userId, eventId));
     }
 
     @PatchMapping("/{eventId}")
-    public EventFullDto updateUserEvent(@PathVariable Long userId, @PathVariable Long eventId,
+    public ResponseEntity<EventFullDto> updateUserEvent(@PathVariable Long userId, @PathVariable Long eventId,
                                         @Valid @RequestBody UpdateEventUserRequest updateRequest) {
         log.info("PATCH /users/{}/events/{}: {}", userId, eventId, updateRequest);
-        return eventService.updateUserEvent(userId, eventId, updateRequest);
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(eventService.updateUserEvent(userId, eventId, updateRequest));
     }
 
 }
