e866151eb0aca42246a9050e19aa732c2b20b1c4
Stanislav <ls.meddy@yandex.ru>
2026-06-28 18:11:19 +0300

    CheckStyle Fix's

--- main-service/src/main/java/ru/practicum/Main.java
+++ main-service/src/main/java/ru/practicum/Main.java
@@ -3,11 +3,7 @@ package ru.practicum;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.client.RestClientException;
-
-import java.util.List;
 
 @RestController
 @SpringBootApplication
